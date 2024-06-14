55cccc70adc1c8ac1ce7d8310082d20c97e142e0
nahuel <nahuelguillermoviera@gmail.com>
2024-06-14 16:22:38 -0300

    Start Project

---
+++ src/main/java/com/nahuel/printersapi/PrintersApiApplication.java
@@ -0,0 +1,13 @@
+package com.nahuel.printersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nahuel/printersapi/PrintersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahuel.printersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
